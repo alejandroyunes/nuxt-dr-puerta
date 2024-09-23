1d8c93aae4bef0c4bfde171a00be611d954311c2
Alejandro Cano <159952966+AlejandroCanoYunes@users.noreply.github.com>
2024-09-23 13:11:57 -0500

    testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,46 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config"
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ["~/assets/main.scss"],
+  modules: [    
+  '@formkit/nuxt',
+  '@pinia/nuxt',
+  '@nuxtjs/seo',
+  '@nuxt/image',
+  '@nuxtjs/i18n'
+],
+app: {
+  head: {
+    htmlAttrs: { lang: 'es' },
+    title: 'Servicios Web Profesionales en Colombia | Páginas Profesionales',
+    titleTemplate: '%s',
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ],
+  },
+  rootAttrs: {
+    id: 'paginas-profesionales',
+  },
+},
+image: {
+  format: ['webp', 'png'],
+  screens: {
+    xs: 320,
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    xxl: 1536,
+    xxxl: 2000,
+  },
+},
+i18n: {
+  vueI18n: './i18n.config.ts' // if you are using custom path, default
+},
+  compatibilityDate: "2024-07-11",
 })
