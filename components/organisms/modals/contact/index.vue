46509a9f8d87a8248aeb99ca5f0f490d2076d765
Alejandro Cano <alejandro.cano@outlook.com>
2024-10-06 13:03:07 -0500

    fixing modals contact

--- components/organisms/modals/contact/index.vue
+++ components/organisms/modals/contact/index.vue
@@ -94,7 +94,7 @@ const submitHandler = async (createForm: Props) => {
             </div>
 
             <div class="form-group">
-              <label for="message">{{ $t('contactMessage') }}</label>
+              <label for="message">{{ $t('contactMessageLabel') }}</label>
               <FormKit
                 type="textarea"
                 name="message"
